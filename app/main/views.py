82dec83dc4c2dcd175a3e9301c1efdccf378b668
Robin <robinkaris7@gmail.com>
2019-09-23 08:39:32 +0300

    this commit will make changes to index route

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,26 @@ from flask_login import login_required,current_user
 import markdown2
 from ..email import mail_message
 from ..request import get_quote
+
+
+@main.route("/",methods=['GET','POST'])
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    posts = Post.query.all()
+    form = SubscriberForm()
+    if form.validate_on_submit():
+        email = form.email.data
+
+        new_subscriber=Subscriber(email=email)
+        new_subscriber.save_subscriber()
+
+        mail_message("Subscription Received","email/welcome_subscriber",new_subscriber.email,subscriber=new_subscriber)
+
+    title = "Welcome to My Blog"
+    
+    name  = "Quote"
+    quote = get_quote()
+    
+    return render_template('index.html',title=title,posts=posts,subscriber_form=form,name=name,quote=quote)
