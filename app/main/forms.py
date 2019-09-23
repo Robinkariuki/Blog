3307fb2d711cfba516c56c0ad5d4870a4999f19a
Robin <robinkaris7@gmail.com>
2019-09-23 08:31:37 +0300

    this commit will add postform class instance

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import Subscriber
+from wtforms import ValidationError
+
+
+class PostForm(FlaskForm):
+    title = StringField("Post Title",validators=[Required()])
+    post = TextAreaField("Write your post here")
+    category = SelectField("Post Category",choices=[('Tech','Tech'),('Travel','Travel'),('Fashion','Fashion'),('Food','Food'),('Life','Life'),('Culture','Culture')],validators=[Required()])
+    submit = SubmitField('Submit')
+    
