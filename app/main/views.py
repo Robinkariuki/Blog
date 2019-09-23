6dc66bd078c35c2c2487ebfde2215841dd412f7f
Robin <robinkaris7@gmail.com>
2019-09-23 08:43:26 +0300

    this commi will modify views

--- app/main/views.py
+++ app/main/views.py
@@ -30,7 +30,7 @@ def index():
     quote = get_quote()
     
     return render_template('index.html',title=title,posts=posts,subscriber_form=form,name=name,quote=quote)
-    
+
 @main.route('/user/<uname>')
 @login_required
 def profile(uname):
@@ -40,3 +40,49 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route("/new_post",methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        new_post=Post(title=title,post=post,category=category)
+
+        new_post.save_post()
+
+        subscribers=Subscriber.query.all()
+
+        for subscriber in subscribers:
+            mail_message("New Blog Post","email/new_post",subscriber.email,post=new_post)
+
+        return redirect(url_for('main.index'))
+
+    title="Make a post"
+    return render_template('new_post.html',title=title,post_form=form)
+
+@main.route("/post/<int:id>",methods=['GET','POST'])
+def post(id):
+    post=Post.query.get_or_404(id)
+    comment = Comment.query.all()
+    form=CommentForm()
+
+    if request.args.get("like"):
+        post.like = post.like+1
+
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect("/post/{post_id}".format(post_id=post.id))
+
+    if form.validate_on_submit():
+        comment=form.comment.data
+        new_comment = Comment(id=id,comment=comment,user_id=current_user.id,post_id=post.id)
+
+        new_comment.save_comment()
+
+        return redirect("/post/{post_id}".format(post_id=post.id))
+
+    return render_template('post.html',post=post,comments=comment,comment_form=form)
